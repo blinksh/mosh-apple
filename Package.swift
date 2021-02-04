4bbb0dc7b85b486ab7474038ad34f1419b2c0c10
Yury Korolev <yury.korolev@gmail.com>
2021-02-04 10:13:42 +0300

    Use symlinks

--- Package.swift
+++ Package.swift
@@ -7,14 +7,14 @@ let package = Package(
   name: "mosh-apple",
   platforms: [.macOS("11")],
   dependencies: [
-    .package(url: "https://github.com/yury/FMake", from : "0.0.15"),
+    .package(url: "https://github.com/yury/FMake", from : "0.0.16"),
     // .package(path: "../FMake")
   ],
   targets: [
     .binaryTarget(
         name: "Protobuf_C_",
-        url: "https://github.com/yury/protobuf-cpp-apple/releases/download/v3.14.0/Protobuf_C_-static.xcframework.zip",
-        checksum: "07433ba7926493200ff7ad31412bc9247d6ddc092b4fa5e650b01c6f36a35559"
+        url: "https://github.com/yury/protobuf-apple/releases/download/v3.14.0/Protobuf_C_-static.xcframework.zip",
+        checksum: "a90dbb75b3ef12224d66cddee28073066e0cab6453f79392d8f954b5904b8790"
     ),
     .target(
       name: "build",
